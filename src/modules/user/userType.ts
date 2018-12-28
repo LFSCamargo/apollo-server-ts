ae5ca0b9dcdb8c28a02debbbeb5c3ac042954724
Luiz Fernando Sousa Camargo <luizepauloxd@gmail.com>
2018-12-28 02:22:58 -0200

    🔥 Removes query and mutation types

--- src/modules/user/userType.ts
+++ src/modules/user/userType.ts
@@ -15,15 +15,4 @@ export default gql`
     count: Int
     edges: [User]
   }
-
-  type Mutation {
-    register(name: String, email: String, password: String): AuthenticationOutput
-    login(email: String, password: String): AuthenticationOutput
-  }
-  
-  type Query {
-    me: User
-    user(_id: String): User
-    users(search: String, first: Int, after: Int): UserConnectionOutput
-  }
 `;
